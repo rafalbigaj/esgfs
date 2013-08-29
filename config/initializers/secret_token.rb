9061cb8f0b6b357fcc2cfe362ffcd5f9bb515a3f
Rafał Bigaj <rafal@ubuntu-vbox.(none)>
2013-08-29 16:10:10 +0200

    Initial 3.2.14 application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Esgfs::Application.config.secret_token = 'dfd06d937c567c3c9b013b3e95e08d88140bf637a18f23de8f4f2fc9d3222a6b873d4a1a9b840bc2c8e13f0fd8002ef36f3c6b0913ab431f0b2b9b0f74f5a713'
